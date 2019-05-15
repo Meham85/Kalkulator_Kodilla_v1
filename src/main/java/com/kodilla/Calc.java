acb6fc89dbfcead9fc0a66105a2e58d0ca0d7b6f
Meham85 <codedtp@gmial.com>
2019-05-15 23:28:05 +0200

    First Calc

---
+++ src/main/java/com/kodilla/Calc.java
@@ -0,0 +1,11 @@
+package com.kodilla;
+
+public class Calc {
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractBFromA(int a, int b) {
+        return a - b;
+    }
+}
